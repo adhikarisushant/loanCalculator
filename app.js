2de7b367adf9050375fb05eb4f5dbc6bf544ec2d
Sushant <sushant.adhikari.619@gmail.com>
2020-12-05 13:51:30 +0545

    UI optimization and loading gif

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+    // Show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+    e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e) {
+function calculateResults() {
     // UI Vars
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
@@ -23,17 +31,26 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+        // show results
+        document.getElementById('results').style.display = 'block';
+
+        // hide loading
+        document.getElementById('loading').style.display = 'none';
     }
     else {
         showError('Please check your numbers');
     }
 
-
-    e.preventDefault();
 }
 
 // Show Error
 function showError(error) {
+      // hide results
+      document.getElementById('results').style.display = 'none';
+
+      // hide loading
+      document.getElementById('loading').style.display = 'none';
     // Create a div
     const errorDiv = document.createElement('div');
 
